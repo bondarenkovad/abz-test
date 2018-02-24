48026e205354096b091290f93e325c7aee96ca9d
Vad Bondarenko <bondarenkowow@gmail.com>
2018-02-24 16:23:11 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="welcome">
+        Hello to all coders!!!
+      </div>
+    );
+  }
+}
+
+export default App;
